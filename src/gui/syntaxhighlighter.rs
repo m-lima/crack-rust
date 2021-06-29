b4001c31187eed58bcebf3749804a1ae07b589fc
Marcelo Lima <marcelowind@gmail.com>
2021-06-29 17:27:06 +0200

    WIP: Getting syntax to actually compile

--- src/gui/syntaxhighlighter.rs
+++ src/gui/syntaxhighlighter.rs
@@ -2,7 +2,7 @@ use cpp::cpp;
 
 use qmetaobject::*;
 
-/// This trait allow to override a Qt QSyntaxHighlighter
+/// This trait allows to override a Qt QSyntaxHighlighter
 pub trait QSyntaxHighlighter: QObject {
     /// Required for the implementation detail of the QObject custom derive
     fn get_object_description() -> &'static QObjectDescription
@@ -16,48 +16,40 @@ pub trait QSyntaxHighlighter: QObject {
         }
     }
 
-    // /// Refer to the Qt documentation of QSyntaxHighlighter::document
-    // fn document(&self) -> Option<QQuickTextDocument> {
-    //     let obj = self.get_cpp_object();
-    //     let text_document = cpp!(unsafe [obj as "Rust_QSyntaxHighlighter *"] -> QQuickTextDocument as "const QQuickTextDocument *" {
-    //         return obj ? obj->quickDocument() : QQuickTextDocument();
-    //     });
-    // }
-
-    // /// Refer to the Qt documentation of QSyntaxHighlighter::setDocument
-    // fn set_document(&mut self, document: QQuickTextDocument) -> QModelIndex {
-    //     let obj = self.get_cpp_object();
-    //     cpp!(unsafe [obj as "Rust_QSyntaxHighlighter *", document as "QQuickTextDocument"] -> QQuickTextDocument as "QQuickTextDocument" {
-    //         return obj ? obj->document() : QQuickTextDocument();
-    //     })
-    // }
-
     fn highlight_block(&mut self, text: String);
 }
 
-// cpp_class!(
-//     #[derive(Default, Clone, Copy, PartialEq)]
-//     pub unsafe struct QQuickTextDocument as "QQuickTextDocument"
-// );
+cpp! {{
+    #include <qmetaobject_rust.hpp>
+    #include <QtQuick/QQuickTextDocument>
+    #include <QtGui/QSyntaxHighlighter>
+}}
 
 cpp! {{
-    #include <QtGui/QSyntaxHighlighter>
-    #include <QtQuick/QQuickTextDocument>
+    struct Rust_QSyntaxHighlighterProxy : public QSyntaxHighlighter {
+        explicit Rust_QSyntaxHighlighterProxy(QObject *parent = nullptr) : QSyntaxHighlighter(parent) {}
+    };
 
-    class Rust_QSyntaxHighlighterProxy : public QSyntaxHighlighter {
-        Q_OBJECT
-
+    class Rust_QSyntaxHighlighter : public RustObject<Rust_QSyntaxHighlighterProxy> {
         Q_PROPERTY(QQuickTextDocument *textDocument READ textDocument WRITE setTextDocument NOTIFY textDocumentChanged)
 
         public:
-        explicit Rust_QSyntaxHighlighterProxy(QObject *parent = nullptr) : QSyntaxHighlighter(parent) {
+        Rust_QSyntaxHighlighter() : RustObject<Rust_QSyntaxHighlighterProxy>() {
             m_TextDocument = nullptr;
         }
 
-        signals:
-        void textDocumentChanged();
+        void highlightBlock(const QString &text) override {
+            return rust!(Rust_QSyntaxHighlighter_highlightBlock [
+                rust_object: QObjectPinned<'_, dyn QSyntaxHighlighter> as "TraitObject",
+                text: QString as "QString"
+            ] {
+                rust_object.borrow_mut().highlight_block(text.clone().into())
+            });
+        }
 
-        protected:
+        QQuickTextDocument *textDocument() const {
+            return m_TextDocument;
+        }
 
         void setTextDocument(QQuickTextDocument *textDocument) {
             if (m_TextDocument == textDocument) {
@@ -71,29 +63,10 @@ cpp! {{
             emit textDocumentChanged();
         }
 
-        QQuickTextDocument *textDocument() {
-            return m_TextDocument;
-        }
+        signals:
+        void textDocumentChanged();
 
+        private:
         QQuickTextDocument *m_TextDocument;
-    };
-}}
-
-cpp! {{
-    #include <qmetaobject_rust.hpp>
-
-    struct Rust_QSyntaxHighlighter : RustObject<Rust_QSyntaxHighlighterProxy> {
-
-        using Rust_QSyntaxHighlighterProxy::setTextDocument;
-        using Rust_QSyntaxHighlighterProxy::textDocument;
-
-        void highlightBlock(const QString &text) override {
-            return rust!(Rust_QSyntaxHighlighter_highlightBlock [
-                rust_object: QObjectPinned<'_, dyn QSyntaxHighlighter> as "TraitObject",
-                text: QString as "QString"
-            ] {
-                rust_object.borrow_mut().highlight_block(text.clone().into())
-            });
-        }
     };
 }}
