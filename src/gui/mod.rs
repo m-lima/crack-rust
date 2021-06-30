85bd07c2f40070975247732c34855c33e690f9e0
Marcelo Lima <marcelowind@gmail.com>
2021-06-30 19:48:49 +0200

    Allow selecting algorithm for regex

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -59,14 +59,14 @@ impl qmetaobject::QSingletonInit for Cracker {
 #[derive(qmetaobject::QObject, Default)]
 struct HashExtractor {
     base: qmetaobject::qt_base_class!(trait QSyntaxHighlighter),
+    useSha256: qmetaobject::qt_property!(bool),
     color: qmetaobject::qt_property!(qmetaobject::QColor),
     hashes: qmetaobject::qt_method!(fn(&self, text: String) -> qmetaobject::QVariantList),
 }
 
 impl QSyntaxHighlighter for HashExtractor {
     fn highlight_block(&mut self, text: String) {
-        let regex = <hash::sha256::Hash as hash::Hash>::regex();
-        regex.find_iter(&text).for_each(|m| {
+        self.regex().find_iter(&text).for_each(|m| {
             let start = m.start();
             let count = m.end() - start;
             self.format_text(start, count, self.color)
@@ -75,10 +75,19 @@ impl QSyntaxHighlighter for HashExtractor {
 }
 
 impl HashExtractor {
+    #[inline]
+    fn regex(&self) -> &regex::Regex {
+        if self.useSha256 {
+            <hash::sha256::Hash as hash::Hash>::regex()
+        } else {
+            <hash::md5::Hash as hash::Hash>::regex()
+        }
+    }
+
     #[allow(clippy::unused_self, clippy::needless_pass_by_value)]
     fn hashes(&self, text: String) -> qmetaobject::QVariantList {
-        let regex = <hash::sha256::Hash as hash::Hash>::regex();
-        let set = regex
+        let set = self
+            .regex()
             .find_iter(&text)
             .map(|m| String::from(m.as_str()))
             .collect::<std::collections::HashSet<_>>();
