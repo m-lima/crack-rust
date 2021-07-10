61e366d2d6321c44ec13202be316ff4e90a3c496
Marcelo Lima <marcelowind@gmail.com>
2021-07-10 13:26:28 +0200

    WIP: Cancel crack with Arc

--- src/gui/cracker.rs
+++ src/gui/cracker.rs
@@ -4,29 +4,31 @@ use crate::channel;
 use crate::decrypt;
 use crate::hash;
 use crate::options;
-use crate::secrets;
 
+#[allow(non_snake_case, dead_code)]
 #[derive(QObject, Default)]
 pub struct Cracker {
     base: qmetaobject::qt_base_class!(trait QObject),
-    running: qmetaobject::qt_property!(bool),
+    running: qmetaobject::qt_property!(bool; READ is_running WRITE set_running NOTIFY runningChanged),
+    runningChanged: qmetaobject::qt_signal!(running: bool),
     progressed: qmetaobject::qt_signal!(progress: u8),
     found: qmetaobject::qt_signal!(input: String, output: String),
     error: qmetaobject::qt_signal!(message: String),
     crack: qmetaobject::qt_method!(
         fn(
-            &self,
+            &mut self,
             prefix: String,
             length: u8,
-            custom_salt: bool,
+            customSalt: bool,
             salt: String,
             useSha256: bool,
-            auto_device: bool,
+            autoDevice: bool,
             useGpu: bool,
-            hashes: qmetaobject::QVariantList,
+            input: String,
             files: qmetaobject::QVariantList,
         )
     ),
+    running_arc: std::sync::Arc<std::sync::atomic::AtomicBool>,
 }
 
 #[allow(
@@ -35,8 +37,21 @@ pub struct Cracker {
     clippy::fn_params_excessive_bools
 )]
 impl Cracker {
+    fn is_running(&self) -> bool {
+        self.running_arc.load(std::sync::atomic::Ordering::Relaxed)
+    }
+
+    fn set_running(&mut self, running: bool) {
+        let prev = self
+            .running_arc
+            .swap(running, std::sync::atomic::Ordering::Relaxed);
+        if prev != running {
+            self.runningChanged(running);
+        }
+    }
+
     fn crack(
-        &self,
+        &mut self,
         prefix: String,
         length: u8,
         custom_salt: bool,
@@ -44,30 +59,9 @@ impl Cracker {
         use_sha256: bool,
         auto_device: bool,
         use_gpu: bool,
-        input: qmetaobject::QVariantList,
+        input: String,
         files: qmetaobject::QVariantList,
     ) {
-        println!(
-            "Prefix: {}, Length: {}, Salt: {}, Algorithm: {}, Device: {}, Hashes: {}, Files: {}",
-            prefix,
-            length,
-            if custom_salt {
-                salt.as_str()
-            } else {
-                secrets::SALT
-            },
-            if use_sha256 { "SHA256" } else { "MD5" },
-            if auto_device {
-                "AUTO"
-            } else if use_gpu {
-                "GPU"
-            } else {
-                "CPU"
-            },
-            input.len(),
-            files.len(),
-        );
-
         if use_sha256 {
             self.crack_algorithm::<hash::sha256::Hash>(
                 prefix,
@@ -94,23 +88,22 @@ impl Cracker {
     }
 
     fn crack_algorithm<H: hash::Hash>(
-        &self,
+        &mut self,
         prefix: String,
         length: u8,
         custom_salt: bool,
         salt: String,
         auto_device: bool,
         use_gpu: bool,
-        input: qmetaobject::QVariantList,
+        input: String,
         files: qmetaobject::QVariantList,
     ) {
         use qmetaobject::QMetaType;
 
-        let input = input
-            .into_iter()
-            .filter_map(|v| qmetaobject::QString::from_qvariant(v.clone()))
-            .filter_map(|s| H::from_str(&s.to_string()).ok())
-            .collect();
+        let input = H::regex()
+            .find_iter(&input)
+            .filter_map(|m| <hash::md5::Hash as hash::Hash>::from_str(m.as_str()).ok())
+            .collect::<std::collections::HashSet<_>>();
 
         let files = files
             .into_iter()
@@ -127,68 +120,66 @@ impl Cracker {
             Some(options::Device::CPU)
         };
 
-        let options = match options::Decrypt::new(
-            input,
-            files,
-            maybe_salt,
-            length,
-            prefix,
-            None,
-            maybe_device,
-        ) {
-            Ok(options) => options,
-            Err(err) => {
-                self.error(err.to_string());
-                return;
-            }
-        };
+        match options::Decrypt::new(input, files, maybe_salt, length, prefix, None, maybe_device) {
+            Ok(options) => self.launch(options),
+            Err(err) => self.error(err.to_string()),
+        }
+    }
 
+    fn launch<H: hash::Hash>(&mut self, options: options::Decrypt<H>) {
         let channel = Channel::new(self);
+        let running_arc = self.running_arc.clone();
 
         std::thread::spawn(move || {
+            running_arc.swap(true, std::sync::atomic::Ordering::Relaxed);
+
             // TODO: Need to communicate failure
             // TODO: Need to communicate Summary?
             let _ignored = decrypt::execute(&options, &channel);
+
+            running_arc.swap(false, std::sync::atomic::Ordering::Relaxed);
         });
     }
 }
 
 struct Channel {
-    progress_callback: Box<dyn Fn(u8) + Send + Sync>,
-    result_callback: Box<dyn Fn((String, String)) + Send + Sync>,
+    progress: Box<dyn Fn(u8) + Send + Sync>,
+    result: Box<dyn Fn((String, String)) + Send + Sync>,
+    running_arc: std::sync::Arc<std::sync::atomic::AtomicBool>,
 }
 
 impl Channel {
     fn new(cracker: &Cracker) -> Self {
         let ptr = qmetaobject::QPointer::from(&*cracker);
         let progress = qmetaobject::queued_callback(move |progress| {
-            if let Some(this) = ptr.as_pinned() {
-                this.borrow().progressed(progress);
+            if let Some(pin) = ptr.as_pinned() {
+                pin.borrow().progressed(progress);
             }
         });
         let ptr = qmetaobject::QPointer::from(&*cracker);
         let result = qmetaobject::queued_callback(move |(input, output)| {
-            if let Some(this) = ptr.as_pinned() {
-                this.borrow().found(input, output)
+            if let Some(pin) = ptr.as_pinned() {
+                pin.borrow().found(input, output)
             }
         });
         Self {
-            progress_callback: Box::new(progress),
-            result_callback: Box::new(result),
+            progress: Box::new(progress),
+            result: Box::new(result),
+            running_arc: cracker.running_arc.clone(),
         }
     }
 }
 
 impl channel::Channel for Channel {
     fn progress(&self, progress: u8) {
-        (self.progress_callback)(progress)
+        (self.progress)(progress)
     }
 
     fn result(&self, input: &str, output: &str) {
-        (self.result_callback)((String::from(input), String::from(output)))
+        (self.result)((String::from(input), String::from(output)))
     }
 
     fn should_terminate(&self) -> bool {
-        false
+        self.running_arc.load(std::sync::atomic::Ordering::Relaxed)
     }
 }
